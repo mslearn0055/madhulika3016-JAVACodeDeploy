a8f1a882ed1cead7a27760e8f2144f38871a32be
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2025-01-09 22:15:51 +0530

    Update fix_privilages.sh

--- scripts/fix_privilages.sh
+++ scripts/fix_privilages.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#!/bin/bash
+chmod +x $PWD/*.sh
+chmod +x $PWD/*.jar
+
 chmod +x /home/ubuntu/server/* jar
 chmod +x /home/ubuntu/server/fix_privilages.sh
 chmod +x /home/ubuntu/server/server_clear.sh
